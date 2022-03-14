f8e0515ce9bd3d70cf004b192c6b9f95e5d37b33
swistek <swistekxd@gmail.com>
2022-03-14 23:22:14 +0100

    .

---
+++ frontend/src/shared/components/FormElements/ImageUpload.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './ImageUpload.css';
+
+const ImageUpload = (props) => {
+  return <div className='form-control'>ImageUpload</div>;
+};
+
+export default ImageUpload;
